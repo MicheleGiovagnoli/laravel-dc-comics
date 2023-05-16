a23035da8340f3a90fea8444817e89f3b71585c7
michele <michele.giovagnoli1@gmail.com>
2023-05-16 15:30:06 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('page-title', 'comics')
+
+@section('content')
+    <main>
+        <p>
+            main
+        </p>
+    </main>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <p>
+            Made With Love By Michele
+        </p>
+    </div>
+</footer>
